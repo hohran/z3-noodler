66234c5872fc0f58bcd1d2f65726442bdb5ffa1e
vhavlena <vohavlena@gmail.com>
2023-11-03 13:54:56 +0100

    length-proc: basic skeleton

--- src/smt/params/theory_str_noodler_params.h
+++ src/smt/params/theory_str_noodler_params.h
@@ -9,6 +9,7 @@ struct theory_str_noodler_params {
     bool m_preprocess_red = false;
     bool m_loop_protect = false;
     bool m_try_nielsen = false;
+    bool m_try_length_proc = false;
 
     theory_str_noodler_params(params_ref const & p = params_ref()) {
         updt_params(p);
