bbe16300c3f2eb0bbecf34c4e86baa18270757da
Jan Hranička <hranickajan@seznam.cz>
2024-03-01 14:23:20 +0100

    testing

--- src/smt/theory_str_noodler/theory_str_noodler.cpp
+++ src/smt/theory_str_noodler/theory_str_noodler.cpp
@@ -857,12 +857,6 @@ namespace smt::noodler {
             }
         }
 
-				std::cerr << "Formula:\n======\n";
-				for (const Predicate& pred : instance.get_predicates()) {
-					std::cerr << pred << std::endl;
-				}
-				std::cerr << "======\n";
-
         // try length-based decision procedure (if enabled) to solve
         if(m_params.m_try_length_proc && LengthDecisionProcedure::is_suitable(instance, aut_assignment)) {
 
